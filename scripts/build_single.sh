5b4c7cf63dbf08311438e18269a24c81e93d7650
Jeff LaBundy <jeff@labundy.com>
2018-09-28 17:56:00 -0500

    scripts: Add single-module build script
    Added a script to cross-compile and copy a single loadable module.
    
    Signed-off-by: Jeff LaBundy <jeff@labundy.com>

---
+++ scripts/build_single.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$(sudo ls /mnt/raspberrypi/boot)" ]
+then
+  sudo sshfs root@raspberrypi:/ /mnt/raspberrypi
+fi
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- $1
+
+mod_path=$(grep -o '".*"' include/generated/utsrelease.h | sed 's/"//g')
+
+sudo cp $1 /mnt/raspberrypi/lib/modules/$mod_path/kernel/$1
